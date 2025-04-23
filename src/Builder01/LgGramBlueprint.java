4ea874d0f509689b20958aa3a2cf1099c5ae5803
marcus_kang <lebuet89@gmail.com>
2025-04-24 00:23:41 +0900

    builder pattern added

---
+++ src/Builder01/LgGramBlueprint.java
@@ -0,0 +1,30 @@
+package Builder01;
+
+public class LgGramBlueprint extends BluePrint {
+
+    private Computer computer;
+
+    public LgGramBlueprint(){
+        computer = new Computer("default", "default", "default");
+    }
+
+    @Override
+    public void setCpu() {
+        computer.setCpu("i7");
+    }
+
+    @Override
+    public void setRam() {
+        computer.setRam("8g");
+    }
+
+    @Override
+    public void setStorage() {
+        computer.setStorage("256g ssd");
+    }
+
+    @Override
+    public Computer getComputer() {
+        return computer;
+    }
+}
