fb67a7099241a828f877ad9d3f418daa61a32950
marcus_kang <lebuet89@gmail.com>
2025-04-23 14:35:59 +0900

    prototype added

---
+++ src/Prototype/Age.java
@@ -0,0 +1,28 @@
+package Prototype;
+
+public class Age {
+    int year;
+    int value;
+
+    public Age(int year, int value) {
+        super();
+        this.year = year;
+        this.value = value;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
