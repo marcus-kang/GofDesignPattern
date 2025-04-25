4272f8951548cd0574a200283cb2886c07685596
marcus_kang <lebuet89@gmail.com>
2025-04-25 17:29:31 +0900

    bridge & composite added

---
+++ src/Bridge/MorseCodeFunction.java
@@ -0,0 +1,7 @@
+package Bridge;
+
+public interface MorseCodeFunction {
+    public void dot();
+    public void dash();
+    public void space();
+}
---
+++ src/Bridge/PrintMorseCode.java
@@ -0,0 +1,33 @@
+package Bridge;
+
+public class PrintMorseCode extends MorseCode{
+    public PrintMorseCode(MorseCodeFunction function) {
+        super(function);
+    }
+
+    //minsung
+    public PrintMorseCode m(){
+        dash();dash();space();
+        return this;
+    }
+    public PrintMorseCode i(){
+        dot();dot();space();
+        return this;
+    }
+    public PrintMorseCode n(){
+        dash();dot();space();
+        return this;
+    }
+    public PrintMorseCode s(){
+        dot();dot();dot();space();
+        return this;
+    }
+    public PrintMorseCode u(){
+        dot();dot();dash();space();
+        return this;
+    }
+    public PrintMorseCode g(){
+        dash();dash();dot();space();
+        return this;
+    }
+}
