8626f87c3b39a77f1f63e38bf1fa21a887c48e6d
marcus_kang <lebuet89@gmail.com>
2025-05-02 17:01:52 +0900

    facade added

---
+++ src/Facade/Application.java
@@ -0,0 +1,12 @@
+package Facade;
+
+
+import SystemPkg.Facade;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Facade facade = new Facade();
+        facade.process();
+    }
+}
