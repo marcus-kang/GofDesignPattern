cf1c6e4045e9057f28f06d9efced70523709e5f1
marcus_kang <lebuet89@gmail.com>
2025-05-03 22:15:37 +0900

    observer added

---
+++ src/Observer/Button2.java
@@ -0,0 +1,10 @@
+package Observer;
+
+import java.util.Observable;
+
+public class Button2 extends Observable {
+    public void onClick(){
+        setChanged();
+        notifyObservers();
+    }
+}
